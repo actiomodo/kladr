594e4a8c7f5d8e3ab8b3fd32bf155f6779d15398
Quovis Modo <quovismodo@protonmail.com>
2020-08-11 03:22:47 +0300

    Add writing decomposed "code" to table

--- kladr.py
+++ kladr.py
@@ -4,21 +4,23 @@ from dataclasses import dataclass
 
 @dataclass
 class kladrDataClass:
+    # rr-ddd-ttt-lll-aa
     region: str     # rr
     district: str   # ddd
     town: str       # ttt
     locality: str   # lll
-    updateCode: str # uu
+    actuality: str  # aa
     level: str      # classification level
 
 @dataclass
 class streetDataClass:
+    # rr-ddd-ttt-lll-ssss-aa
     region: str     # rr
     district: str   # ddd
     town: str       # ttt
     locality: str   # lll
     street: str     # ssss
-    updateCode: str # uu
+    actuality: str  # aa
     level: str      # classification level
 
 def storeDataKladr(value):
@@ -28,11 +30,11 @@ def storeDataKladr(value):
     data.district = value[2:5]
     data.town = value[5:8]
     data.locality = value[8:11]
-    data.updateCode = value[11:]
-    data.level = max([0, 1][int(data.region) > 0],
-                     [0, 2][int(data.district) > 0],
-                     [0, 3][int(data.town) > 0],
-                     [0, 4][int(data.locality) > 0],
+    data.actuality = value[11:]
+    data.level = max(['0', '1'][int(data.region) > 0],
+                     ['0', '2'][int(data.district) > 0],
+                     ['0', '3'][int(data.town) > 0],
+                     ['0', '4'][int(data.locality) > 0],
                      )
     return data
 
@@ -44,9 +46,10 @@ def storeDataStreet(value):
     data.town = value[5:8]
     data.locality = value[8:11]
     data.street = value[11:15]
-    data.updateCode = value[15:]
-    data.level = 5
+    data.actuality = value[15:]
+    data.level = '5'
     return data
+
 
 def getInfoKladr():
     """ query data """
@@ -100,6 +103,70 @@ def getInfoStreet():
         if conn is not None:
             conn.close()
 
+def codeKladrDecomposition():
+    commands = (
+        """
+        DROP TABLE IF EXISTS kladr_code_tbl
+        """,
+        """ 
+        CREATE TABLE kladr_code_tbl (
+        code_id serial,
+        code TEXT, 
+        region TEXT DEFAULT '', 
+        district TEXT DEFAULT '', 
+        town TEXT DEFAULT '', 
+        locality TEXT DEFAULT '', 
+        actuality TEXT DEFAULT '', 
+        level TEXT DEFAULT ''
+        )
+        """)
+    db.executeCommand(commands)
+    """ query data """
+    query = """
+        SELECT code
+        FROM kladr_tbl
+        LIMIT 5
+    """
+    query = """
+        SELECT code
+        FROM kladr_tbl
+    """
+    conn = None
+    try:
+        params = db.config()
+        conn = db.psycopg2.connect(**params)
+        cur = conn.cursor(cursor_factory=db.RealDictCursor)
+        cur.execute(query)
+        for row in db.iterRow(cur, 10):
+            rowValues = [row[desc] for desc in row]
+            dataKladr = storeDataKladr(row['code'])
+            valuesToInsert = [row['code'], 
+                              dataKladr.region, dataKladr.district, 
+                              dataKladr.town, dataKladr.locality, 
+                              dataKladr.actuality, dataKladr.level]
+            valuesToInsert = {
+                'code' : row['code'], 
+                'region' : dataKladr.region, 
+                'district' : dataKladr.district, 
+                'town' : dataKladr.town, 
+                'locality' : dataKladr.locality, 
+                'actuality' : dataKladr.actuality, 
+                'level' : dataKladr.level
+            }
+            queryInsert = """
+                INSERT INTO kladr_code_tbl 
+                (code, region, district, town, locality, actuality, level)
+                VALUES (%(code)s, %(region)s, %(district)s, %(town)s, 
+                %(locality)s, %(actuality)s, %(level)s)
+            """
+            db.executeCommandWithParameters([(queryInsert, valuesToInsert)])
+        cur.close()
+    except (Exception, db.psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
 if __name__ == '__main__':
     query = """
         SELECT *
@@ -115,5 +182,6 @@ if __name__ == '__main__':
         LIMIT 10
     """
     #db.getInfo(query)
-    getInfoKladr()
-    getInfoStreet()
+    #getInfoKladr()
+    #getInfoStreet()
+    codeKladrDecomposition()
