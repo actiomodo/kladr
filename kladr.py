405254eab944dac55dc4b3d716c5f94ea1922776
Quovis Modo <quovismodo@protonmail.com>
2020-08-12 23:02:32 +0300

    Rewrite dataclasses and decomposition

--- kladr.py
+++ kladr.py
@@ -13,16 +13,33 @@ class streetDataClass:
     actuality: str = '00'   # aa
     level: str = '0'        # classification level
     
-    def set(self, value):
+    def set(self, valueCode):
         """ decomposition code field of street_tbl """
-        self.region = value[:2]
-        self.district = value[2:5]
-        self.town = value[5:8]
-        self.locality = value[8:11]
-        self.street = value[11:15]
-        self.actuality = value[15:]
+        self.region = valueCode[:2]
+        self.district = valueCode[2:5]
+        self.town = valueCode[5:8]
+        self.locality = valueCode[8:11]
+        self.street = valueCode[11:15]
+        self.actuality = valueCode[15:]
         self.level = '5'
         return self
+
+    def __str__(self):
+        return '-'.join([self.region, self.district, self.town, self.locality,
+                         self.street, self.actuality, self.level
+                         ])
+        
+    def getCode(self):
+        return ''.join([self.region, self.district, self.town, self.locality,
+                         self.street, self.actuality
+                         ])
+        
+    def setValue(self, value, indent):
+        if not isinstance(value, int):
+            return '<?>'
+        if not isinstance(indent, int):
+            return '<?>'
+        return ''.join(['0000', str(int(value))])[-int(indent):]
     
 @dataclass
 class kladrDataClass:
@@ -39,18 +56,49 @@ class kladrDataClass:
                          self.actuality, self.level
                          ])
         
+    def getRegion(self):
+        return ''.join([self.region] + ['0']*15)[:13]
+    
+    def getDistrict(self):
+        return ''.join([self.region, self.district] + ['0']*15)[:13]
+    
+    def getTown(self):
+        return ''.join([self.region, self.district, 
+                        self.town] + ['0']*15
+                       )[:13]
+    
+    def getLocality(self):
+        return ''.join([self.region, self.district, self.town, 
+                        self.locality] + ['0']*15
+                       )[:13]
+    
+    def getOnlyCode(self):
+        return ''.join([self.region, self.district, self.town, 
+                        self.locality]
+                       )[:13]
+
     def getCode(self):
         return ''.join([self.region, self.district, self.town, self.locality,
                          self.actuality
                          ])
+    
+    def getCodeWithoutActuality(self):
+        return self.getCode()[:-2]
         
-    def set(self, value):
+    def setValue(self, value, indent):
+        if not isinstance(value, int):
+            return '<?>'
+        if not isinstance(indent, int):
+            return '<?>'
+        return ''.join(['0000', str(int(value))])[-int(indent):]
+    
+    def set(self, valueCode):
         """ decomposition code field of kladr_tbl """
-        self.region = value[:2]
-        self.district = value[2:5]
-        self.town = value[5:8]
-        self.locality = value[8:11]
-        self.actuality = value[11:]
+        self.region = valueCode[:2]
+        self.district = valueCode[2:5]
+        self.town = valueCode[5:8]
+        self.locality = valueCode[8:11]
+        self.actuality = valueCode[11:]
         self.level = max(['0', '1'][int(self.region) > 0],
                          ['0', '2'][int(self.district) > 0],
                          ['0', '3'][int(self.town) > 0],
@@ -58,12 +106,13 @@ class kladrDataClass:
                          )
         return self
 
-def getInfoKladr():
+def getInfoKladr(testing = False):
     """ query data """
     query = """
         SELECT *
         FROM kladr_tbl
-        LIMIT 1
+        TABLESAMPLE SYSTEM(1) 
+        LIMIT 3
     """
     conn = None
     try:
@@ -76,14 +125,22 @@ def getInfoKladr():
         for row in db.iterRow(cur, 10):
             rowValues = [row[desc] for desc in row]
             print(rowValues)
-            print(storeDataKladr(row['code']))
             dataKladr = kladrDataClass()
-            dataKladr.town='111'
-            print(dataKladr)
-            print(dataKladr.getCode())
-            dataKladr.set(row['code'])
-            print(dataKladr)
-            print(dataKladr.getCode())
+            print(dataKladr.set(row['code']))
+            if testing:
+                dataKladr = kladrDataClass()
+                title = 'new'
+                print(f'{title: <12}: {dataKladr}')
+                dataKladr.town = dataKladr.setValue('a', 3)
+                title = 'town=22'
+                print(f'{title: <12}: {dataKladr}')
+                title = 'getCode'
+                print(f'{title: <12}: {dataKladr.getCode()}')
+                dataKladr.set(row['code'])
+                title = 'set'
+                print(f'{title: <12}: {dataKladr}')
+                title = 'getCode'
+                print(f'{title: <12}: {dataKladr.getCode()}')
         cur.close()
     except (Exception, db.psycopg2.DatabaseError) as error:
         print(error)
@@ -96,6 +153,13 @@ def getInfoStreet():
     query = """
         SELECT *
         FROM street_tbl
+        TABLESAMPLE SYSTEM(1) 
+        LIMIT 5
+    """
+    query = """
+        SELECT *
+        FROM street_tbl
+        ORDER BY random() 
         LIMIT 5
     """
     conn = None
@@ -104,12 +168,13 @@ def getInfoStreet():
         conn = db.psycopg2.connect(**params)
         cur = conn.cursor(cursor_factory=db.RealDictCursor)
         cur.execute(query)
-        print("query: ", query)
-        print("cursor size: ", cur.rowcount)
+        #print("query: ", query)
+        #print("cursor size: ", cur.rowcount)
         for row in db.iterRow(cur, 10):
             rowValues = [row[desc] for desc in row]
             print(rowValues)
-            print(storeDataStreet(row['code']))
+            dataStreet = streetDataClass()
+            print(dataStreet.set(row['code']))
         cur.close()
     except (Exception, db.psycopg2.DatabaseError) as error:
         print(error)
@@ -126,6 +191,7 @@ def codeKladrDecomposition():
         CREATE TABLE kladr_code_tbl (
         code_id serial,
         code TEXT, 
+        onlycode TEXT DEFAULT '', 
         region TEXT DEFAULT '', 
         district TEXT DEFAULT '', 
         town TEXT DEFAULT '', 
@@ -160,18 +226,19 @@ def codeKladrDecomposition():
                               dataKladr.town, dataKladr.locality, 
                               dataKladr.actuality, dataKladr.level]
             valuesToInsert = {
-                'code' : row['code'], 
-                'region' : dataKladr.region, 
-                'district' : dataKladr.district, 
-                'town' : dataKladr.town, 
-                'locality' : dataKladr.locality, 
+                'code' : row['code'],
+                'onlycode' : dataKladr.getOnlyCode(), 
+                'region' : dataKladr.getRegion(), 
+                'district' : dataKladr.getDistrict(), 
+                'town' : dataKladr.getTown(), 
+                'locality' : dataKladr.getLocality(), 
                 'actuality' : dataKladr.actuality, 
                 'level' : dataKladr.level
             }
             queryInsert = """
                 INSERT INTO kladr_code_tbl 
-                (code, region, district, town, locality, actuality, level)
-                VALUES (%(code)s, %(region)s, %(district)s, %(town)s, 
+                (code, onlycode, region, district, town, locality, actuality, level)
+                VALUES (%(code)s, %(onlycode)s, %(region)s, %(district)s, %(town)s, 
                 %(locality)s, %(actuality)s, %(level)s)
             """
             db.executeCommandWithParameters([(queryInsert, valuesToInsert)])
@@ -197,6 +264,6 @@ if __name__ == '__main__':
         LIMIT 10
     """
     #db.getInfo(query)
-    getInfoKladr()
+    #getInfoKladr()
     #getInfoStreet()
-    #codeKladrDecomposition()
+    codeKladrDecomposition()
